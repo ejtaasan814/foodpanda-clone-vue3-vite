502bdf33021a81deaff908964ed3e7065e90dbc5
EJ Von Taasan <ejvon@clearmindai.com>
2023-07-06 16:42:39 +0800

    Update page title

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,10 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        title: 'Foodpanda'
+      }
     },
     {
       path: '/about',
@@ -15,9 +18,17 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: 'About-us'
+      }
     }
   ]
 })
+
+const DEFAULT_TITLE = 'Foodpanda';
+router.beforeEach((to) => {
+  document.title = to.meta.title || DEFAULT_TITLE;
+});
 
 export default router
